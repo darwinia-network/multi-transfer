f8476c894f784f7b18e50bb862eaf556ce0c5794
fewensa <fewensa@protonmail.com>
2022-05-19 17:26:05 +0800

    for remember

--- scripts/transferx.sh
+++ scripts/transferx.sh
@@ -5,11 +5,19 @@
 BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
 WORK_PATH=${BIN_PATH}/../
 
-SEED=$1
+##
+# export SEED=0x12345
+# export ENDPOINT=wss://darwiniacrab-rpc.dwellir.com
+# ./scripts/transferx.sh ./assets/transferx/step-0.csv
+###
+
 SEED=${SEED:-0x1}
+ENDPOINT=${ENDPOINT:-wss://pangolin-rpc.darwinia.network}
+FILE=${DATA_FILE:-$1}
 
 ${WORK_PATH}/multi-transfer.sh transferx \
+  --endpoint ${ENDPOINT} \
   --seed ${SEED} \
-  --file ./data/transferx.csv
+  --file ${FILE}
 
 
